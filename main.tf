42598df43bf871fe04ca7d7cd8ac374ec251c6d2
amulya1974 <amulya1.cloudservice@gmail.com>
2024-12-27 23:25:10 +0530

    updated

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "Demo" {
-  ami           = "ami-07a1dfb9cd95e3c0a"
+  ami           = "ami-09ce8ceb8d1d8e730"
   instance_type = "t2.micro"
-  key_name = "bhola"
+  key_name = "test"
   tags = {
     Name = "Demo"
   }
